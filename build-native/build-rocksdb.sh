9a757f99777d6ecf622e615dc71d01c508a2062e
rafael-aero <8791811+rafael-aero@users.noreply.github.com>
2019-12-07 01:29:17 +0100

    Update build-rocksdb.sh

--- build-native/build-rocksdb.sh
+++ build-native/build-rocksdb.sh
@@ -151,7 +151,8 @@ else
         brew install zlib
         brew install bzip2
         brew install gflags
-        brew install gcc48 --use-llvm
+        brew install gcc48
+        brew install llvm
     else
         echo "Linux detected"
         CFLAGS=-static-libstdc++
@@ -190,8 +191,8 @@ else
         export LDFLAGS
         export ROCKSDB_DISABLE_GFLAGS=1
         (. ./build_tools/build_detect_platform detected~; {
-            grep detected~ -e '-DSNAPPY' &> /dev/null || fail "failed to detect snappy, install libsnappy-dev"
             grep detected~ -e '-DZLIB' &> /dev/null || fail "failed to detect zlib, install libzlib-dev"
+            grep detected~ -e '-DSNAPPY' &> /dev/null || fail "failed to detect snappy, install libsnappy-dev"
             grep detected~ -e '-DLZ4' &> /dev/null || fail "failed to detect lz4, install liblz4-dev"
             grep detected~ -e '-DZSTD' &> /dev/null || fail "failed to detect zstd, install libzstd-dev"
             grep detected~ -e '-DGFLAGS' &> /dev/null && fail "gflags detected, see https://github.com/facebook/rocksdb/issues/2310" || true
