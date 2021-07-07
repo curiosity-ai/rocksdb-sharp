526bb8d30956af133edbfa0f16a2b2b8b09bbf92
theolivenbaum <rafael@rafael.aero>
2021-07-07 11:52:47 +0200

    Update build-rocksdb.sh

--- build-native/build-rocksdb.sh
+++ build-native/build-rocksdb.sh
@@ -60,7 +60,7 @@ if [[ $OSINFO == *"MSYS"* || $OSINFO == *"MINGW"* ]]; then
         checkout "snappy" "$SNAPPYREMOTE" "$SNAPPYVERSION" "$SNAPPYVERSION"
         mkdir -p build
         (cd build && {
-            cmake -G "Visual Studio 16 2019" -DSNAPPY_BUILD_TESTS=0 -DENABLE_AVX=0 -DENABLE_AVX2=0 .. || fail "Running cmake on snappy failed"
+            cmake -G "Visual Studio 16 2019" -DSNAPPY_BUILD_TESTS=0 -DENABLE_AVX=0 -DENABLE_AVX2=0 -DSNAPPY_REQUIRE_AVX=0 -DSNAPPY_REQUIRE_AVX2=0 .. || fail "Running cmake on snappy failed"
             update_vcxproj || warn "unable to patch snappy for static vc runtime"
         }) || fail "cmake build generation failed"
 
