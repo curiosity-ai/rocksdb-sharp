9b9bfe5b5a67dc2e26da66489dddfa4b89324e63
theolivenbaum <rafael@rafael.aero>
2023-04-19 12:05:33 +0200

    Update build-rocksdb.sh

--- build-native/build-rocksdb.sh
+++ build-native/build-rocksdb.sh
@@ -151,7 +151,7 @@ else
         LIBEXT=.so
         RUNTIME=linux-x64
         # Linux Dependencies    
-        sudo apt-get install libsnappy-dev libbz2-dev libz-dev liblz4-dev libzstd-dev
+        sudo apt-get install libsnappy-dev libbz2-dev libz-dev liblz4-dev libzstd-dev || (apt-get update && apt-get install libsnappy-dev libbz2-dev libz-dev liblz4-dev libzstd-dev)
     fi
     
     mkdir -p rocksdb || fail "unable to create rocksdb directory"
