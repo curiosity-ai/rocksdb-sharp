35d588f382e8913e39180a018c02d53d28c4beb9
Dejan Grofelnik Pelzel <dejan@bunnycdn.com>
2023-08-20 16:05:36 +0700

    PoC of replication finished

---
+++ csharp/RocksDbSharp.Replication/Backup/BackupEngineOptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocksDbSharp.Backup
+{
+    public class BackupEngineOptions : IDisposable
+    {
+        public IntPtr Handle { get; set; }
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Where to keep the DB files. Has to be different than the DB name.
+        /// </summary>
+        /// <param name="path"></param>
+        public BackupEngineOptions(string path)
+        {
+            Path = path;
+            Handle = Native.Instance.rocksdb_backup_engine_options_create(path);
+        }
+
+        ~BackupEngineOptions()
+        {
+            Native.Instance.rocksdb_backup_engine_options_destroy(Handle);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ csharp/RocksDbSharp.Replication/Backup/RestoreOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocksDbSharp.Backup
+{
+    public class RestoreOptions : IDisposable
+    {
+        public IntPtr Handle { get; set; }
+
+        public RestoreOptions()
+        {
+            Handle = Native.Instance.rocksdb_restore_options_create();
+        }
+
+        ~RestoreOptions()
+        {
+            Native.Instance.rocksdb_restore_options_destroy(Handle);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
